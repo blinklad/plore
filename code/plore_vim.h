13f706be4c9b30695404d756f542ab5e9af12e0a
Evan Harvey <evan.harvey@utas.edu.au>
2021-12-09 22:47:18 +1100

    Wrap vim command function in typedef

--- code/plore_vim.h
+++ code/plore_vim.h
@@ -236,7 +236,8 @@ global vim_binding VimBindings[] = {
 };
 
 
-typedef void vim_command_function(plore_state *State, plore_vim_context *VimContext, plore_file_context *FileContext, vim_command Command);
-#define PLORE_VIM_COMMAND(CommandName) void Do##CommandName(plore_state *State, plore_vim_context *VimContext, plore_file_context *FileContext, vim_command Command)
+#define PLORE_VIM_COMMAND_FUNCTION(Name) void Name(plore_state *State, plore_vim_context *VimContext, plore_file_context *FileContext, vim_command Command)
+typedef PLORE_VIM_COMMAND_FUNCTION(vim_command_function);
+#define PLORE_VIM_COMMAND(CommandName) PLORE_VIM_COMMAND_FUNCTION(Do##CommandName)
 
 #endif //PLORE_VIM_H
