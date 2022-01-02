46d3fc3f89689986df821098caaff4372de4a51a
Evan Harvey <evan.harvey@utas.edu.au>
2022-01-02 22:59:10 +1100

    Fix typo in BakedData declaration that prevents multiple fonts.

--- code/plore_meta.c
+++ code/plore_meta.c
@@ -24,15 +24,10 @@ main(int ArgCount, char **Args) {
 	}
 	
 	f32 Heights[] = {
-		32.0f,
-#if 0
-		8.0f, 10.0f, 12.0f, 14.0f, 16.0f, 18.0f, 20.0f,
-		22.0f, 24.0f, 26.0f, 28.0f, 30.0f, 32.0f, 34.0f, 36.0f,
-		40.0f, 44.0f, 48.0f, 52.0f, 60.0f, 64.0f, 68.0f, 72.0f,
-#endif
+		24.0f, 25.0f, 26.0f, 27.0f, 28.0f, 29.0f, 30.0f, 32.0f, 33.0f, 35.0f, 37.0f, 39.0f, 41.0f, 42.0f, 44.0f,
 	};
 	u8 *Bitmaps[ArrayCount(Heights)] = {0};
-	stbtt_bakedchar BakedData[GLYPH_COUNT][ArrayCount(Heights)] = {0};
+	stbtt_bakedchar BakedData[ArrayCount(Heights)][GLYPH_COUNT] = {0};
 	
 	
 	FILE *FontFile = fopen(Font, "r");
