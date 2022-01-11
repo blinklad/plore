1c8ba7b2bdf3d4545700c208a2483cfae08007e8
Evan Harvey <evan.harvey@utas.edu.au>
2022-01-11 13:19:40 +1100

    Commit registry reading weirdness!

--- build.bat
+++ build.bat
@@ -9,15 +9,16 @@ pushd .\build
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 SET CommonCompilerFlags= -DEBUG:full -nologo -fp:fast -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4146 -wd4201 -wd4100 -wd4189 -wd4505 -wd4244 -wd4456 -wd4100 -wd4013  -wd4996 -wd4204 -wd4221 -wd4116 -DPLORE_INTERNAL=1 -DPLORE_WINDOWS=1 -FC -Z7
-SET LinkerFlags= -DEBUG:full -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib opengl32.lib glu32.lib ksuser.lib shlwapi.lib Shell32.lib
+
+SET LinkerFlags= -DEBUG:full -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib opengl32.lib glu32.lib ksuser.lib shlwapi.lib Shell32.lib 
 
 del *.pdb > NUL 2> NUL
 
 REM NOTE(Evan): Compile and run the metaprogram.
 IF "%1"=="meta" (
 	@echo on
-	cl %CommonCompilerFlags% ..\code\plore_meta.c -Fmplore_meta.map /link -incremental:no -opt:ref -PDB:plore_meta.pdb
-	plore_meta.exe
+	cl %CommonCompilerFlags% ..\code\plore_meta.c -Fmplore_meta.map /link Advapi32.Lib -incremental:no -opt:ref -PDB:plore_meta.pdb || exit
+	plore_meta.exe || exit
 	@echo off
 )
 
