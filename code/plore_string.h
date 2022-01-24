d6319b6c25972a2f5e372ba8185029d856590009
Evan Harvey <harveyem@utas.edu.au>
2022-01-24 17:40:31 +1100

    Partially correct variadic macro handling using a GCC extension - needs a guard for MSVC.

--- code/plore_string.h
+++ code/plore_string.h
@@ -6,17 +6,17 @@
 #endif
 #include "stb_sprintf.h"
 
-#define StringPrint(Buffer, Format, ...) stbsp_sprintf(Buffer, Format, __VA_ARGS__)
-#define StringPrintSized(Buffer, Size, Format, ...) stbsp_snprintf(Buffer, Size, Format, __VA_ARGS__)
+#define StringPrint(Buffer, Format, ...) stbsp_sprintf(Buffer, Format, ##__VA_ARGS__)
+#define StringPrintSized(Buffer, Size, Format, ...) stbsp_snprintf(Buffer, Size, Format, ##__VA_ARGS__)
 
 
 internal u64
 StringLength(char *S) {
 	Assert(S);
-	
+
 	u64 Result = 0;
 	while (*S++) Result++;
-	
+
 	return(Result);
 }
 
@@ -70,11 +70,11 @@ AlphaToLower(char C) {
 	if (!IsAlpha(C)) {
 		return(C);
 	}
-	
+
 	if (C >= 'A' && C <= 'Z') {
 		C += 32;
 	}
-	
+
 	return(C);
 }
 
@@ -94,17 +94,17 @@ typedef struct substring_result {
 internal substring_result
 SubstringHelper(char *S, char *Substring, b64 CaseSensitive) {
 	substring_result Result = {0};
-	
+
 	if (!S || !Substring) return(Result);
-	
+
 	u64 SLen = StringLength(S);
 	u64 SubLen = StringLength(Substring);
 	if (SubLen > SLen) return(Result);
-	
+
 	for (u64 I = 0; I < SLen; I++) {
 		if ((SLen - I) < SubLen) break;
 		Result.Index = I;
-		
+
 		char *A = S+I;
 		char *B = Substring;
 		for (u64 J = 0; J < SubLen; J++) {
@@ -114,21 +114,21 @@ SubstringHelper(char *S, char *Substring, b64 CaseSensitive) {
 				aTest = ToLower(aTest);
 				bTest = ToLower(bTest);
 			}
-			
+
 			if (aTest != bTest) break;
 			if (J == SubLen-1) Result.IsContained = true;
 		}
-		
+
 		if (Result.IsContained) break;
 	}
-	
+
 	return(Result);
 }
 
 internal substring_result
 SubstringNoCase(char *S, char *Substring) {
-	substring_result Result = SubstringHelper(S, Substring, false); 
-	
+	substring_result Result = SubstringHelper(S, Substring, false);
+
 	return(Result);
 }
 
@@ -142,56 +142,56 @@ Substring(char *S, char *Substring) {
 internal b64
 StringCompare(char *A, char *B) {
 	b64 Result = false;
-	
+
 	while (*A && *B) {
 		if (*A != *B) {
 			if (*B > *A) Result = false;
 			else         Result = true;
-			
+
 			break;
 		} else *A++, *B++;
 	}
-	
+
 	return(Result);
 }
 
 internal b64
 StringCompareReverse(char *A, char *B) {
 	b64 Result = false;
-	
+
 	u64 LenA = StringLength(A);
 	u64 LenB = StringLength(B);
-	
+
 	while (LenA-- && LenB--) {
 		if (A[LenA] != B[LenB]) {
 			if (B[LenB] > A[LenA]) Result = false;
 			else                   Result = true;
-			
+
 			break;
 		}
 	}
-	
+
 	return(Result);
 }
 
 internal char *
 StringConcatenate(char *Left, u64 LeftBufferSize, char *Right) {
 	char *Result = Left;
-	
+
 	u64 Length = StringLength(Left);
 	if (!LeftBufferSize)            return (Result);
 	if (Length == LeftBufferSize-1) return(Result);
-	
+
 	Left += Length;
 	u64 Count = LeftBufferSize -= Length;
-	
+
 	while (*Right) {
 		*Left++ = *Right++;
 		if (!Count--) break;
 	}
-	
+
 	if (*Left) *Left = '\0';
-	
+
 	return(Result);
 }
 
@@ -205,7 +205,7 @@ StringsAreEqualIgnoreCase(char *A, char *B) {
 			return(false);
 		}
 	}
-	
+
 	if ((!*A && *B) || (*A && !*B)) return(false);
 	return(true);
 }
@@ -215,7 +215,7 @@ StringsAreEqual(char *A, char *B) {
 	while (*A && *B) {
 		if (*A++ != *B++) return(false);
 	}
-	
+
 	if ((!*A && *B) || (*A && !*B)) return(false);
 	return(true);
 }
@@ -224,22 +224,22 @@ plore_inline u64
 StringCopy(char *Source, char *Destination, u64 BufferSize) {
 	u64 BytesWritten = 0;
 	Assert(Source && Destination && BufferSize);
-	
+
 	// NOTE(Evan): We return bytes written, *not* including the null terminator,
 	// and currently allow empty strings.
 	if (!*Source) {
 		*Destination = '\0';
 		return BytesWritten;
 	}
-	
+
 	while (BytesWritten < BufferSize) {
 		Destination[BytesWritten++] = *Source++;
 		if (!*Source) {
 			break;
 		}
-		
+
 	}
-	
+
 	if (Destination == Source) Assert(0);
 	// NOTE(Evan): Truncate the string.
 	if (BytesWritten >= BufferSize) {
@@ -247,7 +247,7 @@ StringCopy(char *Source, char *Destination, u64 BufferSize) {
 	} else {
 		Destination[BytesWritten] = '\0';
 	}
-	
+
 	return(BytesWritten);
 }
 // credit: stb
@@ -255,12 +255,12 @@ StringCopy(char *Source, char *Destination, u64 BufferSize) {
 #define ROTATE_RIGHT(val, n)  (((val) >> (n)) | ((val) << (sizeof(u64)*8 - (n))))
 
 // TODO(Evan): Better hash function.
-plore_inline u64 
+plore_inline u64
 HashString(char *String) {
 	u64 Seed = 0x31415926;
 	u64 Hash = Seed;
 	while (*String) Hash = ROTATE_LEFT(Hash, 9) + (u8) *String++;
-	
+
 	// Thomas Wang 64-to-32 bit mix function, hopefully also works in 32 bits
 	Hash ^= Seed;
 	Hash = (~Hash) + (Hash << 18);
@@ -272,12 +272,12 @@ HashString(char *String) {
 	return(Hash+Seed);
 }
 
-plore_inline u64 
+plore_inline u64
 HashBytes(u8 *Bytes, u64 Count) {
 	u64 Seed = 0x31415926;
 	u64 Hash = Seed;
 	while (Count--) Hash = ROTATE_LEFT(Hash, 9) + *Bytes++;
-	
+
 	// Thomas Wang 64-to-32 bit mix function, hopefully also works in 32 bits
 	Hash ^= Seed;
 	Hash = (~Hash) + (Hash << 18);
@@ -303,12 +303,12 @@ TempString(memory_arena *Arena, u64 Capacity) {
 		.Buffer = PushBytes(Arena, Capacity),
 		.Capacity = Capacity,
 	};
-	
+
 	return(Result);
 }
 
-#define TempCat(S, Format, ...)   S.Count += StringPrintSized(S.Buffer+S.Count, S.Capacity, Format, __VA_ARGS__)
-#define TempPrint(S, Format, ...) S.Count = StringPrintSized(S.Buffer, S.Capacity, Format, __VA_ARGS__)
+#define TempCat(S, Format, ...)   S.Count += StringPrintSized(S.Buffer+S.Count, S.Capacity, Format, ##__VA_ARGS__)
+#define TempPrint(S, Format, ...) S.Count = StringPrintSized(S.Buffer, S.Capacity, Format, ##__VA_ARGS__)
 
 
 #endif //PLORE_STRING_H
