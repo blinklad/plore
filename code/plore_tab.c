0251946e024379da45462cfbe5e933bd50687126
Evan Harvey <harveyem@utas.edu.au>
2022-01-31 10:10:37 +1100

    Change Map interface to allow set initialization - 0-sized types are nonstandard.

--- code/plore_tab.c
+++ code/plore_tab.c
@@ -2,7 +2,7 @@ internal plore_tab *
 GetCurrentTab(plore_state *State) {
 	Assert(State->TabCurrent < ArrayCount(State->Tabs));
 	plore_tab *Result = State->Tabs + State->TabCurrent;
-	
+
 	return(Result);
 }
 
@@ -11,31 +11,31 @@ internal void
 TabInit(plore_state *State, plore_tab *Tab) {
 	Assert(!Tab->Active);
 	Assert(!Tab->Arena.BytesUsed);
-	
+
 	State->TabCount++;
 	Tab->Active = true;
-	
+
 	Tab->FileContext = PushStruct(&Tab->Arena, plore_file_context);
-	
+
 	// NOTE(Evan): Don't show hidden files by default!
 	Tab->FilterState = PushStruct(&Tab->Arena, plore_file_filter_state);
-	Tab->FilterState->HideMask.HiddenFiles = true; 
-	
+	Tab->FilterState->HideMask.HiddenFiles = true;
+
 	Tab->FilterState->MetadataDisplay = FileMetadataDisplay_Basic;
-	
+
 	Tab->DirectoryState = PushStruct(&Tab->Arena, plore_current_directory_state);
 	Tab->FileContext->FileInfo = MapInit(&Tab->Arena, plore_path, plore_file_listing_info, 512);
-	Tab->FileContext->Selected = MapInit(&Tab->Arena, plore_path, void, 256);
+	Tab->FileContext->Selected = SetInit(&Tab->Arena, plore_path, 256);
 }
 
 internal void
 ClearTab(plore_state *State, u64 TabIndex) {
 	Assert(TabIndex < ArrayCount(State->Tabs));
 	Assert(State->TabCount > 1);
-	
+
 	State->TabCount--;
 	plore_tab *Tab = State->Tabs + TabIndex;
-	
+
 	memory_arena Arena = Tab->Arena;
 	ZeroArena(&Arena);
 	*Tab = ClearStruct(plore_tab);
@@ -45,27 +45,27 @@ ClearTab(plore_state *State, u64 TabIndex) {
 internal b64
 SetCurrentTab(plore_state *State, u64 NewTab) {
 	b64 Result = false;
-	
+
 	Assert(NewTab < ArrayCount(State->Tabs));
 	plore_tab *Tab = State->Tabs + NewTab;
 	plore_tab *Previous = State->Tabs + State->TabCurrent;
 	if (Previous != Tab) {
 		if (!Tab->Active) TabInit(State, Tab);
-		
+
 		Result = true;
-		
+
 		State->TabCurrent = NewTab;
 		Platform->SetCurrentDirectory(Tab->CurrentDirectory.Absolute);
-		
+
 		//
 		// NOTE(Evan): Setting a new tab will always be a frame late.
 		// Otherwise, if a Button() or Window() call stores a pointer to a plore_file, Synchronize() will change the underlying
 		// backing store for that string - i.e., we're fucked!
 		// This could be solved in other ways, but it's not worth the complexity for now. Just render at least 60fps.
 		//
-		//SynchronizeCurrentDirectory(Tab);		
-		
+		//SynchronizeCurrentDirectory(Tab);
+
 	}
-	
+
 	return(Result);
 }
